a79b4085a834e0a7a011c2b53fe56cf970aeab45
Ilya Betyaev <110731177+ilya90906767@users.noreply.github.com>
2024-03-15 14:33:12 +0300

    Commit from TeleportHQ

--- src/views/catalog-v22.js
+++ src/views/catalog-v22.js
@@ -30,11 +30,24 @@ const CatalogV22 = (props) => {
         <CardForCatalog rootClassName="card-for-catalog-root-class-name4"></CardForCatalog>
         <CardForCatalog rootClassName="card-for-catalog-root-class-name5"></CardForCatalog>
       </div>
+      <div className="catalog-v22-container3">
+        <div className="catalog-v22-container4">
+          <button type="button" className="catalog-v22-button button">
+            <span className="catalog-v22-text3">--&gt;</span>
+          </button>
+        </div>
+        <div className="catalog-v22-container5">
+          <button type="button" className="catalog-v22-button1 button">
+            <span className="catalog-v22-text4">&lt;--</span>
+          </button>
+        </div>
+        <div className="catalog-v22-container6"></div>
+      </div>
       <div className="catalog-v22-footer">
         <footer className="catalog-v22-footer1">
           <div className="catalog-v22-separator"></div>
-          <div className="catalog-v22-container3">
-            <span className="catalog-v22-text3">
+          <div className="catalog-v22-container7">
+            <span className="catalog-v22-text5">
               © 2024 VETRICO, All Rights Reserved.
             </span>
           </div>
